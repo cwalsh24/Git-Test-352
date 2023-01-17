7d32eae10411657e67c505e05e5f11e0c51b0eae
cwalsh24 <cwalsh24@EPSG1-825038>
2023-01-17 13:38:53 -0600

    Add project files.

---
+++ CW2 Getting into Git/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW2_Getting_into_Git
+{
+    class Program
+    {
+        private int steve;
+        
+        public void setSteve(int s)
+        {
+            steve = s; 
+        }
+
+        public int getSteve()
+        {
+            return steve; 
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world " + 5);
+            //Console.ReadKey();
+            Console.WriteLine("setting steve to 4");
+            Program p = new Program(); 
+            p.setSteve(4);
+            Console.WriteLine("Steve is: " + p.getSteve());
+            Console.ReadKey();
+        }
+    }
+}
